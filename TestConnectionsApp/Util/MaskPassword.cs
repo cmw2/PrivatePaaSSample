0b6df845e42e0b49978ba25fbb99d7166f808d12
Chris Westbrook <chwestbr@microsoft.com>
2023-07-25 11:54:00 -0400

    add come comments and VS Code startup files

--- TestConnectionsApp/Util/MaskPassword.cs
+++ TestConnectionsApp/Util/MaskPassword.cs
@@ -4,6 +4,13 @@ namespace TestConnectionsApp.Util
 {
     public class MaskPassword
     {
+        /// <summary>
+        /// Masks the password in the given connection string by replacing it with asterisks.
+        /// </summary>
+        /// <param name="connectionString">The connection string to mask.</param>
+        /// <param name="firstSeparator">The separator used to split the connection string into parts. Default is ";"</param>
+        /// <param name="secondSeparator">The separator used to split each part into key-value pairs. Default is "="</param>
+        /// <returns>The masked connection string.</returns>
         public static string Mask(string connectionString, string firstSeparator = ";", string secondSeparator = "=")
         {
             var parts = connectionString.Split(firstSeparator);
